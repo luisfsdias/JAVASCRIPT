10afd80a6d0d70fa39587afb934cb5d559e11ced
Luis Felipe Santos Dias <luisdias1506@gmail.com>
2023-05-05 22:12:00 -0300

    exercicios

---
+++ aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimp(n)
+{
+    if(n%2 == 0){
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+
+console.log(parimp(223))
+
+//let res = parimp(4)
+//console.log(`${res}`)
